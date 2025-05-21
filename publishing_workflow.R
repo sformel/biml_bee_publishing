0f0eeccd6e238edc8c22b83af129ac1e99e7fda2
sformel <steve@formeldataservices.com>
2025-05-20 19:31:39 -0500

    heavy revision to work flow documentation

--- publishing_workflow.R
+++ publishing_workflow.R
@@ -5,11 +5,6 @@ source('scripts/BIML_QAQC.R')
 
 # Run filter_and_join_tables_BIML_all.R -----------------------------------
 
-# 1. Remove all records that do not have confirmed species-level identifications present,
-# 2. Reassign the value in the email field to “BIML” if the value does not match any of the other datasetID values in the PIT table,
-# 3. Join the details of the PIT table to the records (1:N) based on matching values in the email field of the flat file and the datasetID field in the PIT table, and
-# 4. Export the new .csv file, titled “species_project_joined_new.csv” for use in the second R script.
-
 source('scripts/filter_and_join_tables_BIML_all.R')
 
 # crosswalk data and write out for publication ---------------------------
